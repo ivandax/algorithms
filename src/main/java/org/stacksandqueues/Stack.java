2fe9006b56f161a52748d0fd144eab547483c836
ivandax <ivandax_89@hotmail.com>
2024-03-23 19:47:12 +0100

    Implement iterator on stack

--- src/main/java/org/stacksandqueues/Stack.java
+++ src/main/java/org/stacksandqueues/Stack.java
@@ -1,7 +1,9 @@
 package org.stacksandqueues;
 
+import java.util.Iterator;
+
 // Implements Stack using Linked list (node)
-public class Stack<T> implements StackInterface<T> {
+public class Stack<T> implements Iterable<T> {
 
     private class Node {
         T item;
@@ -10,7 +12,6 @@ public class Stack<T> implements StackInterface<T> {
 
     Node first = null;
 
-    @Override
     public void push(T item) {
         Node oldFirst = first;
         first = new Node();
@@ -18,24 +19,41 @@ public class Stack<T> implements StackInterface<T> {
         first.next = oldFirst;
     }
 
-    @Override
     public T pop() {
         T item = first.item;
         first = first.next;
         return item;
     }
 
-    @Override
     public boolean isEmpty() {
         return first == null;
     }
 
-    @Override
     public void printAsArray() {
-        Node current = this.first;
-        while(current != null){
-            System.out.println(current.item);
+        // This is possible because the Stack implements the iterator
+        for(T item : this){
+            System.out.println(item);
+        }
+    }
+
+    public Iterator<T> iterator() {
+        return new ListIterator();
+    }
+
+    private class ListIterator implements Iterator<T> {
+        private Node current = first;
+
+        public boolean hasNext() {
+            return current != null;
+        }
+
+        public T next(){
+            T item = current.item;
             current = current.next;
+            return item;
+        }
+
+        public void remove() {
         }
     }
 }
