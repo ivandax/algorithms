2c6d06bbfd93d8845e1adb3d74c9a7e51ebdd494
Ivan <ivandariogye@gmail.com>
2026-07-09 21:42:02 +0200

    Implement first version of solver

--- src/main/java/org/puzzle/Solver.java
+++ src/main/java/org/puzzle/Solver.java
@@ -23,7 +23,7 @@ public class Solver {
         }
     }
 
-    class PriorityFunction implements Comparator<SearchNode> {
+    static class PriorityFunction implements Comparator<SearchNode> {
         @Override
         public int compare(SearchNode nodeA, SearchNode nodeB) {
             return Integer.compare(nodeA.board.hamming(), nodeB.board.hamming());
@@ -33,18 +33,27 @@ public class Solver {
     private void stackSolution(SearchNode searchNode) {
         Stack<Board> stack = new Stack<>();
         stack.push(searchNode.board);
-        while (searchNode.previous != null) {
-            stackSolution(searchNode.previous);
+        SearchNode current = searchNode;
+        while (current.previous != null) {
+            stack.push(current.previous.board);
+            current = current.previous;
         }
         this.solutionBoards = stack;
     }
 
-    private void attemptSolution(SearchNode searchNode) {
-        if (searchNode.board.isGoal()) {
+    private boolean attemptSolution(MinPQ<SearchNode> pq) {
+        SearchNode min = pq.delMin();
+        if (min.board.isGoal()) {
             solvable = true;
-            stackSolution(searchNode);
+            stackSolution(min);
+            return true;
         } else {
-
+            Iterable<Board> boards = min.board.neighbors();
+            for (Board board : boards) {
+                SearchNode node = new SearchNode(board, min.moves + 1, min);
+                pq.insert(node);
+            }
+            return attemptSolution(pq);
         }
     }
 
@@ -54,7 +63,8 @@ public class Solver {
         }
         SearchNode root = new SearchNode(initial, 0, null);
         MinPQ<SearchNode> pq = new MinPQ<>(new PriorityFunction());
-        attemptSolution(root);
+        pq.insert(root);
+        attemptSolution(pq);
     }
 
     public Iterable<Board> solution() {
@@ -66,11 +76,12 @@ public class Solver {
     }
 
     public static void main(String[] args) {
-        int[][] sample = {{1, 2, 3}, {4, 5, 6}, {7, 8, 0}};
+        int[][] sample = {{1, 2, 3}, {4, 5, 6}, {0, 7, 8}};
         Board newBoard = new Board(sample);
         Solver mySolver = new Solver(newBoard);
         System.out.println(mySolver.solvable);
         for (Board board : mySolver.solutionBoards) {
+            System.out.println("Printing solutions");
             System.out.println(board.toString());
         }
     }
