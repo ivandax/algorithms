753badf4891e660b0ed1f110d213cac3e4c4c10f
Ivan <ivandariogye@gmail.com>
2026-07-12 11:52:19 +0200

    Add attempt parallel solutions

--- src/main/java/org/puzzle/Solver.java
+++ src/main/java/org/puzzle/Solver.java
@@ -66,14 +66,46 @@ public class Solver {
         }
     }
 
+    private boolean attemptParallelSolutions(MinPQ<SearchNode> mainPq, MinPQ<SearchNode> twinPq) {
+        SearchNode mainMin = mainPq.delMin();
+        SearchNode twinMin = twinPq.delMin();
+        if (twinMin.board.isGoal()) {
+            solvable = false;
+            solutionBoards = null;
+            return false;
+        } else if (mainMin.board.isGoal()) {
+            solvable = true;
+            stackSolution(mainMin);
+            return true;
+        } else {
+            Iterable<Board> mainNeighbors = mainMin.board.neighbors();
+            for (Board board : mainNeighbors) {
+                if (mainMin.previous != null && board.equals(mainMin.previous.board)) continue;
+                SearchNode node = new SearchNode(board, mainMin.moves + 1, mainMin, board.manhattan() + mainMin.moves + 1);
+                mainPq.insert(node);
+            }
+            Iterable<Board> twinNeighbors = twinMin.board.neighbors();
+            for (Board board : twinNeighbors) {
+                if (twinMin.previous != null && board.equals(twinMin.previous.board)) continue;
+                SearchNode node = new SearchNode(board, twinMin.moves + 1, twinMin, board.manhattan() + twinMin.moves + 1);
+                twinPq.insert(node);
+            }
+            return attemptParallelSolutions(mainPq, twinPq);
+        }
+    }
+
     public Solver(Board initial) {
         if (initial == null) {
             throw new IllegalArgumentException("null initializer");
         }
         SearchNode root = new SearchNode(initial, 0, null, initial.manhattan());
-        MinPQ<SearchNode> pq = new MinPQ<>(new PriorityFunction());
-        pq.insert(root);
-        attemptSolution(pq);
+        Board twinBoard = initial.twin();
+        SearchNode twinNode = new SearchNode(twinBoard, 0, null, twinBoard.manhattan());
+        MinPQ<SearchNode> mainPq = new MinPQ<>(new PriorityFunction());
+        MinPQ<SearchNode> twinPq = new MinPQ<>(new PriorityFunction());
+        mainPq.insert(root);
+        twinPq.insert(twinNode);
+        attemptParallelSolutions(mainPq, twinPq);
     }
 
     public Iterable<Board> solution() {
